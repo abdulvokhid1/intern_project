8d717dfbc660a0ba137fbf19257471a24ea5d4c7
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-09-08 03:03:28 +0900

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 const App: FC = () => {
-  return <div className="App">Welcome To React!</div>;
+  return <div className="text-red-500">Welcome To React!</div>;
 };
 
 export default App;
