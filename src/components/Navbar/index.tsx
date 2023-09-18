59ded2a7abc27ab05a718505f06d98240b2f4285
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-09-19 02:55:51 +0900

    done

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,31 @@
 import type { FC } from "react";
+import {
+  LoginOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
 
 const Navbar: FC = () => {
+  const icon_style: string = "cursor-pointer text-[20px]";
   return (
-    <div className="w-[80%] m-auto flex justify-between">
+    <div className="w-[80%] m-auto flex justify-between items-center h-[90px]">
       <div>
         <img />
+        <h3>logo</h3>
       </div>
-      <div>
-        <h3>Home</h3>
-        <h3>Blog</h3>
+      <div className="flex gap-4">
+        <h3 className="cursor-pointer">Home</h3>
+        <h3 className="cursor-pointer">Blog</h3>
       </div>
-      <div>
-        <img />
-        <button>Login</button>
+      <div className=" flex gap-[30px] items-center">
+        <SearchOutlined className={`${icon_style}`} />
+        <BellOutlined className={`${icon_style}`} />
+        <ShoppingCartOutlined className={`${icon_style}`} />
+        <button className="text-white w-[100px] h-[35px] bg-[#46A358] flex justify-center items-center gap-2">
+          <LoginOutlined />
+          Login
+        </button>
       </div>
     </div>
   );
